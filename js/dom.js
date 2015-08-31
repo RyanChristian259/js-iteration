7c253e40d555abed0308a76dd6b2a6c118f7275f
Ryan Ciecalone <ryan@ryanchristianmedia.com>
2015-08-31 16:52:46 -0600

    Completed code for prices exercise

--- js/dom.js
+++ js/dom.js
@@ -9,3 +9,48 @@ Hints for the first problem:
     - takes the value returned by the `sum` function and displays it on the page
 
 */
+
+//shows user the sum of all amounts in box
+var prices = document.getElementsByClassName('all-prices');
+var postAll = document.getElementById('sum-all').addEventListener('click', function(){
+document.getElementById('answer').innerHTML= ('The sum of all amounts is $' + sumAll(prices) + '.00');
+    });
+
+//shows user how many options they have selected
+document.getElementById('count-selected').addEventListener('click', function(){
+  var selected = document.getElementsByTagName('select')[0].selectedOptions;
+  document.getElementById("answer").innerHTML = "You have selected " + selected.length + " amount(s).";
+});
+
+//shows user the total of the amounts they have selected
+document.getElementById('sum-selected').addEventListener('click', function(){
+  var selected = document.getElementsByTagName('select')[0].selectedOptions;
+  document.getElementById("answer").innerHTML = "The total of the amounts you selected is $" + sumSelected(selected) + '.00';
+});
+
+//shows user the average of all amounts in box
+var prices = document.getElementsByClassName('all-prices');
+var postAll = document.getElementById('average-all').addEventListener('click', function(){
+document.getElementById('answer').innerHTML= ('The average of all amounts is $' + averageAll(prices));
+    });
+
+//shows user the average of the amounts they have selected
+document.getElementById('average-selected').addEventListener('click', function(){
+  var selected = document.getElementsByTagName('select')[0].selectedOptions;
+  document.getElementById("answer").innerHTML = "The average of the amounts you selected is $" + averageSelected(selected);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/logic.js
+++ js/logic.js
@@ -1,12 +1,51 @@
-/*
 
-Hints for the first problem:
+// Hints for the first problem:
 
-  1. Define a function that:
-    - is assigned to a variable named `sum`
-    - has one parameter named `options`
-    - returns the calculated sum of all of the values of all of the options passed in
+//   1. Define a function that:
+//     - is assigned to a variable named `sum`
+//     - has one parameter named `options`
+//     - returns the calculated sum of all of the values of all of the options passed in
+// NOTE: use a loop to make this happen.
+// - Users can see the sum of all prices
 
-NOTE: use a loop to make this happen.
 
-*/
+//function to sum all of the prices in the main list
+function sumAll(prices){
+  var total = 0;
+  for (var i = 0; i < prices[0].length; i++) {
+    total += parseInt(prices[0][i].value);
+  }
+  return total;
+}
+
+//function to return the sum of prices selected from the list
+function sumSelected(selected){
+  var total = 0;
+  for (var i = 0; i < selected.length; i++) {
+    total += parseInt(selected[i].value);
+  }
+  return total;
+}
+
+//function to average all of the prices in the main list
+function averageAll(prices){
+  var total = 0;
+  for (var i = 0; i < prices[0].length; i++) {
+    total += parseInt(prices[0][i].value);
+  }
+    total = total / prices[0].length;
+  return total.toFixed(2);
+}
+
+//function to average all of the prices in the main list
+function averageSelected(selected){
+  var total = 0;
+  for (var i = 0; i < selected.length; i++) {
+    total += parseInt(selected[i].value);
+  }
+    total = total / selected.length;
+  return total.toFixed(2);
+}
+// - Users can see the number of prices that are selected (the count)
+// - Users can see the sum of the selected prices
+// - Users can see the average of all of the prices
